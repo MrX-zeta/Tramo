ff50abe16bd30bb12c3fdcf3d747575c574fd224
MrX-zeta <brianluisruizperez@gmail.com>
2026-07-08 15:33:48 -0600

    chore: versionCode 2 / versionName 1.1 para la segunda release
    Incluye celebracion de meta, mejoras de tareas, informe, widget y correcciones varias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.luis.tramo"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
